a57b8b9b4bc101f365adf79f729cd39cd761c438
Vladimir Novikov <valdemar.const@gmail.com>
2020-01-22 17:26:06 +0300

    fix interface

--- Data structures/linked_list/include/linked_list.h
+++ Data structures/linked_list/include/linked_list.h
@@ -1,5 +1,4 @@
-#ifndef NV_LIST_H
-#define NV_LIST_H
+#pragma once
 
 #include <stdbool.h>
 
@@ -12,7 +11,8 @@ typedef struct list_t {
 	list_node *head;
 } nv_list;
 
-nv_list *init_list(nv_list **list);
+nv_list *init_list();
+nv_list *delete_list(nv_list *list);
 
 void insert_to_list(nv_list *list, int new_value);
 void remove_from_list(nv_list *list, int value);
@@ -20,7 +20,3 @@ void remove_from_list(nv_list *list, int value);
 bool is_in_list(nv_list *list, int value);
 
 void print_list(nv_list *list);
-
-nv_list *delete_list(nv_list **list);
-
-#endif //NV_LIST_H
