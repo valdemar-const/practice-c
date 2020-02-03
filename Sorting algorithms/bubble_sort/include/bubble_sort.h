3f63379c915d5677932d26ddd70167784065568c
Vladimir Novikov <valdemar.const@gmail.com>
2020-02-03 16:28:38 +0300

    fix doc comment

--- Sorting algorithms/bubble_sort/include/bubble_sort.h
+++ Sorting algorithms/bubble_sort/include/bubble_sort.h
@@ -1,3 +1,11 @@
 #include <stddef.h>
 
+/**
+ * @brief sorting an array by bubble sort algorithm
+ * 
+ * @param first start data pointer
+ * @param number number of elements
+ * @param size element size in bytes
+ * @param comparator function to compare elements
+ */
 void bsort (void *first, size_t number, size_t size, int (* comparator)(const void *, const void *));
--- Sorting algorithms/bubble_sort/src/bubble_sort.c
+++ Sorting algorithms/bubble_sort/src/bubble_sort.c
@@ -2,14 +2,6 @@
 
 static void swap(void *lhs, void *rhs, size_t size);
 
-/**
- * @brief sorting an array by bubble sort algorithm
- * 
- * @param first start data pointer
- * @param number number of elements
- * @param size element size in bytes
- * @param comparator function to compare elements
- */
 void bsort(void *first, size_t number, size_t size,
 	   int (*comparator)(const void *, const void *))
 {
