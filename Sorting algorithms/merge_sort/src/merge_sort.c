b36b8b235682c2e3d65299b944baf791f61a2a11
Vladimir <vladimir.novikov.k@yandex.ru>
2020-02-09 12:48:21 +0300

    add merge sort implementation

---
+++ Sorting algorithms/merge_sort/src/merge_sort.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include "merge_sort.h"
+
+static void swap(void *lhs, void *rhs, size_t size);
+static void merge(void *left, size_t left_number, void *right,
+		  size_t right_number, size_t size,
+		  int (*comp)(const void *, const void *));
+
+void msort(void *first, size_t number, size_t size,
+	   int (*comp)(const void *, const void *))
+{
+	if (number == 1) {
+		return;
+	}
+	char *begin = (char *)first;
+	size_t left_size = number - number / 2;
+	size_t right_size = number - left_size;
+	char *middle = begin + left_size * size;
+	msort(begin, left_size, size, comp);
+	msort(middle, right_size, size, comp);
+	merge(begin, left_size, middle, right_size, size, comp);
+}
+
+static void swap(void *lhs, void *rhs, size_t size)
+{
+	char *first = lhs;
+	char *second = rhs;
+	do {
+		char tmp = *first;
+		*first++ = *second;
+		*second++ = tmp;
+	} while (--size > 0);
+}
+
+static void merge(void *left, size_t left_number, void *right,
+		  size_t right_number, size_t size,
+		  int (*comp)(const void *, const void *))
+{
+	size_t buff_size = (left_number + right_number) * size;
+	char *buffer = (char *)malloc(buff_size);
+	char *p_buffer = buffer;
+	char *p_left = (char *)left;
+	char *p_right = (char *)right;
+	char *result = (char *)left;
+	while (left_number && right_number) {
+		if (comp(p_left, p_right) <= 0) {
+			memcpy(p_buffer, p_left, size);
+			p_left += size;
+			p_buffer += size;
+			--left_number;
+		} else {
+			memcpy(p_buffer, p_right, size);
+			p_right += size;
+			p_buffer += size;
+			--right_number;
+		}
+	}
+	size_t remainder = abs(left_number - right_number);
+	char *rem_src =
+		(remainder && left_number > right_number) ? p_left : p_right;
+	while (remainder) {
+		memcpy(p_buffer, rem_src, size);
+		rem_src += size;
+		p_buffer += size;
+		--remainder;
+	}
+	memcpy(result, buffer, buff_size);
+	free(buffer);
+}
